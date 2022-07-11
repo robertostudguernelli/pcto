8ce6ef3a95100d4720354cecf160ccf42875d8f8
Roberto Stud. Guernelli <804513@stud.unive.it>
2022-07-11 21:19:48 +0200

    v0.0

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = 'you-will-never-guess'
     
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'DATABASE_URL = postgresql://pcto:password@192.168.0.202:5432/pcto'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-        DBMS_USERNAME = os.environ.get('DBMS_USERNAME') or 'postgres'
-    DBMS_PASSWORD = os.environ.get('DBMS_PASSWORD') or 'password'
-    DBMS_HOST     = os.environ.get('DBMS_HOST')     or '192.168.0.202'
-    DBMS_DB       = os.environ.get('DBMS_DB')       or 'pcto'
+    DBMS_USERNAME = 'postgres'
+    DBMS_PASSWORD = 'password'
+    DBMS_HOST     = '192.168.0.202'
+    DBMS_DB       = 'pcto'
