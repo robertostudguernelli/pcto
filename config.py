49382718a0688c6b4c3fd124318ced02e30a3157
Roberto Stud. Guernelli <804513@stud.unive.it>
2022-07-11 21:28:48 +0200

    v0.0

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SECRET_KEY = 'you-will-never-guess'
     
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = 'DATABASE_URL = postgresql://pcto:password@192.168.0.202:5432/pcto'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://pcto:password@192.168.0.202:5432/pcto'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     DBMS_USERNAME = 'postgres'
