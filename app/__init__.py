8fa82f4220484c46db3344cd5ace476616ea7407
Roberto Stud. Guernelli <804513@stud.unive.it>
2022-07-13 21:18:01 +0200

    v0.0

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
+from asyncio.log import logger
 from flask import Flask
 from flask_sqlalchemy import *
-from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 
+import logging
+logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s : %(message)s')
+
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
-metadata = MetaData()
+engine = create_engine('postgresql://postgres:password@192.168.0.202:5432/pcto', echo = True)
+
 login = LoginManager(app)
 login.login_view = 'login'
 
--- pcto.py
+++ pcto.py
@@ -4,4 +4,4 @@ from app.models import User, Post
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'metadata': metadata, 'User': User, 'Post': Post, 'Person': Person}
+    return {'db': db, 'User': User, 'Post': Post, 'Person': Person}
