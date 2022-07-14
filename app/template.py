d9b65d33d4b47f0f88994d448702436530b13d11
Roberto Stud. Guernelli <804513@stud.unive.it>
2022-07-14 08:50:38 +0200

    v0.0

---
+++ app/template.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+
+app = Flask('__name__')
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/users/<username>')
+def show_profile(username):
+    users = ['alice', 'bob', 'charlie']
+    if username in users:
+        return render_template('profile.html', user=username)
+    else:
+        return render_template('profile.html', rigistered_users=users)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user = request.form["user"]
+    return redirect(url_for('show_profile', username=user))
+
+# prove libere
+
+@app.route('/pippo')
+def hello_pippo():
+    return 'Hello, Pippo!'
+
+@app.route('/appname')
+def appname():
+    return '__name__ -> ' + format(__name__)
+
+@app.route('/divbyzero')
+def divbyzero():
+    num = 8
+    den = 0
+    return num/den
+
+@app.route('/userbyindex/<username>')
+def userbyindex(username):
+    return render_template('index.html', user=username)
