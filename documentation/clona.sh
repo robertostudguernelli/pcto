b8f722a5e5e5419ae8768e10a26dccf7cf3efc34
Roberto Stud. Guernelli <804513@stud.unive.it>
2022-08-06 14:38:17 +0200

    v0.0

---
+++ documentation/clona.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# clona il repository PCTO
+# cambia l'ip del web server per adattarlo alla macchina debian
+# crea venv, lo popola e lo attiva
+# 14 luglio 2022
+
+cd ~
+rm -fR pcto
+git clone https://github.com/robertostudguernelli/pcto
+pip install --upgrade pip
+cd pcto/app
+rpl 202 203 '*/.flaskenv'
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
