9598ab7163986a90dbc5dad8452ca3d4fcd9b93e
Roberto Stud. Guernelli <804513@stud.unive.it>
2022-08-06 15:47:10 +0200

    v0.0

--- microblog-0.15/config.py
+++ microblog-0.15/config.py
@@ -10,12 +10,12 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.libero.it'
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'pcto.rguernelli@libero.it'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '!Pass1word$'
+    ADMINS = ['pcto.rguernelli@libero.it']
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     POSTS_PER_PAGE = 25
