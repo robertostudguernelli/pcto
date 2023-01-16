df8ddf388bcb144481f34ba0ca2cd3c26eef89b4
Roberto Stud. Guernelli <804513@stud.unive.it>
2023-01-16 15:18:09 +0100

    v0.0

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
 from app import app
+from flask import render_template
+
+title='PCTO Roberto Guernelli, 804513?'
 
 @app.route('/')
 @app.route('/index')
+
 def index():
-        return "Hello world"
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title=title, user=user)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
