8bb22d0caca4b94bc21e852758b94db2c0372f73
Roberto Stud. Guernelli <804513@stud.unive.it>
2023-01-16 14:46:26 +0100

    v0.0

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/pcto/Scrivania/progetto/pcto/pcto/venv"
+set -gx VIRTUAL_ENV "/home/pcto/Scrivania/pcto/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
